4e398589f7056347dc3ef31cbf27de160479f186
quicksilverri <rina.iakhina@gmail.com>
2022-10-07 15:31:35 +0500

    sin and cos ver 1.0

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] angles = {30, 15, 37, 115}; 
+
+            foreach (double angle in angles)
+            {
+                double sin = Math.Round(Math.Sin(angle * Math.PI / 180), 3); 
+                double cos = Math.Round(Math.Cos(angle * Math.PI / 180), 3); 
+                Console.WriteLine("sin({2})={0}, cos({2})={1}", sin, cos, angle); 
+            }
+        }
+    }
+}
