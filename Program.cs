8e0103f28b32d230eab67baa7324c9bee37a6136
quicksilverri <rina.iakhina@gmail.com>
2022-10-09 12:53:27 +0500

    HW4 done

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MathFormula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, write x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            double sin = Math.Sin(Math.Sqrt(Math.Pow(x, 2) + 4) / 2);
+            double cos = Math.Cos(Math.Sqrt(Math.Pow(x, 2) + 1) / 2); 
+
+            Console.WriteLine("Answer: {0}", Math.Round(2 * sin * cos, 3)); 
+        }
+    }
+}
