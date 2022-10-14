431855ae4a184dd9a873885a41b52da8f30751b4
quicksilverri <rina.iakhina@gmail.com>
2022-10-14 16:11:50 +0500

    Method PrintSinCos

--- Program.cs
+++ Program.cs
@@ -6,14 +6,26 @@ namespace Classwork
     {
         static void Main(string[] args)
         {
-            int[] angles = {30, 15, 37, 115}; 
+            // int[] angles = {30, 15, 37, 115}; 
 
-            foreach (double angle in angles)
-            {
-                double sin = Math.Round(Math.Sin(angle * Math.PI / 180), 3); 
-                double cos = Math.Round(Math.Cos(angle * Math.PI / 180), 3); 
-                Console.WriteLine("sin({2})={0}, cos({2})={1}", sin, cos, angle); 
-            }
+            // foreach (double angle in angles)
+            // {
+            //     PrintSinCos(angle);
+            // }
+
+            Console.WriteLine("Please, insert the angle value in degrees:  "); 
+            double angle_in_degrees = Double.Parse(Console.ReadLine());
+            double angle_in_rads = angle_in_degrees * Math.PI / 180;
+
+            PrintSinCos(angle_in_rads);
+            
+        }
+
+        static void PrintSinCos(double x)
+        {
+            double sin = Math.Round(Math.Sin(x), 3); 
+            double cos = Math.Round(Math.Cos(x), 3); 
+            Console.WriteLine("sin({2})={0}, cos({2})={1}", sin, cos, Math.Round(x, 3));
         }
     }
 }
