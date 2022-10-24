30166fb60cbe8eb9763da73efc0d9ea521d633f4
quicksilverri <rina.iakhina@gmail.com>
2022-10-24 17:42:19 +0500

    HW5 done

---
+++ Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateFormula(2, 3) + CalculateFormula(5, 7));
+        }
+        
+        static double CalculateFormula(double x, double y)
+        {
+            double top = Math.Sin(x) + Math.Sin(y);
+            double bottom = Math.Cos(x) + Math.Cos(y);
+            
+            return Math.Round(top / bottom, 3);
+
+        }
+    }
+}
